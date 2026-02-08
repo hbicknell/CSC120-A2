a92aa47d4626ec254e46d8e79c28c51fea84aa7e
Hayden Bicknell <hbicknell@smith.edu>
2026-02-08 14:01:41 -0500

    initializing work?

--- computer.py
+++ computer.py
@@ -1,10 +1,26 @@
 class Computer:
 
     # What attributes will it need?
+    description: str = ""
+    processor_type: str = ""
+    hard_drive_capacity: int = 0
+    memory: int = 0
+    operating_system: str = ""
+    year_made: int = 0
+    price: int = 0 
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, description: str, processor: str, hard_drive: int,
+                memory: int, OS: str, year: int, price: int):
+        self.description = description 
+        self.processor = processor
+        self.hard_drive_capacity = hard_drive
+        self.memory = memory 
+        self.operating_system = OS
+        self.year_made = year
+        self.price = price
+
+       
 
     # What methods will you need?
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,6 +1,8 @@
+from computer import *
 class ResaleShop:
 
     # What attributes will it need?
+    inventory = []
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
@@ -8,3 +10,9 @@ class ResaleShop:
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
+    #def buy(self)
+        #call compuer() constructor 
+        #to create a new computer instance 
+
+        #call inventory.append to add the 
+        #new computer instance ot the inventory
