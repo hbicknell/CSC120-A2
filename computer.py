f160c2d4977d1f0831efbd538df5b9ed2dc014b7
Hayden Bicknell <hbicknell@smith.edu>
2026-02-08 15:20:42 -0500

    computer with correct attributes and running

--- computer.py
+++ computer.py
@@ -21,6 +21,26 @@ class Computer:
         self.year_made = year
         self.price = price
 
-       
-
     # What methods will you need?
+
+    def computer_info (self):
+         return f"{self.description} -- Processor Type: {self.processor_type} Hard Drive Capacity: {self.hard_drive_capacity} Memory: {self.memory} Operating System: {self.operating_system} Made: {self.year_made} Price:  {self.price}"
+    
+    def refurbish(self, new_os: str):
+        self.operating_system = new_os
+        return self.operating_system
+    
+    def update_price(self, new_price: int):
+        self.price = new_price
+        return self.price
+
+
+def main(): 
+    myComputer:Computer = Computer("hi", "heya", 13, 23, "haydens", 2007, 0)
+    print(myComputer.price)
+    print(myComputer.computer_info())
+    myComputer.update_price(30)
+    print(myComputer.computer_info())
+
+if __name__ == "__main__":
+    main()
